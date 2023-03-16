d8cb3b9f752bffd63fda429b2a9d2c7f82606e23
Masha Moreva <mashamiskova@yandex.ru>
2023-03-16 19:26:09 +0300

    добавлена логика работы с сервером

--- todo-app.js
+++ todo-app.js
@@ -43,7 +43,7 @@
         return list;
     }
 
-    function createTodoItem(name) {
+    function createTodoItemElement(todoItem, { onDone, onDelete }) {
         let item = document.createElement('li');
 
         let buttonGroup = document.createElement('div');
@@ -53,7 +53,7 @@
         let random = (Math.random() * (11 - 1 + 1)) + 1;
         item.id = random.toFixed(2);
         item.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-center');
-        item.textContent = name;
+        item.textContent = todoItem.name;
 
         buttonGroup.classList.add('btn-group', 'btn-group-sm');
         doneButton.classList.add('btn', 'btn-success', 'mr-1');
@@ -61,61 +61,10 @@
         deleteButton.classList.add('btn', 'btn-danger');
         deleteButton.textContent = 'Удалить';
 
-        buttonGroup.append(doneButton, deleteButton);
-        item.append(buttonGroup);
-
-        return {
-            item,
-            doneButton,
-            deleteButton,
-            buttonGroup,
-        };
-    }
-
-    function createTodoApp(container, title, listName) {
-        let todoAppTitle = createAppTitle(title);
-        let todoItemForm = createTodoForm();
-        let todoList = createTodoList();
-
-        container.append(todoAppTitle, todoItemForm.form, todoList);
-
-        if (localStorage.getItem(listName)) {
-            todoArray = JSON.parse(localStorage.getItem(listName));
-
-            for (let object of todoArray) {
-                let todoItem = createTodoItem(todoItemForm.input.value);
-                todoItem.item.id = object.id;
-                todoItem.item.textContent = object.name;
-                if (object.done == true) {
-                    todoItem.item.classList.add('list-group-item-success');
-                } else {
-                    todoItem.item.classList.remove('list-group-item-success');
-                }
-
-
-                changeItemStatus(todoItem.item, todoItem.doneButton);
-                deleteItem(todoItem.item, todoItem.deleteButton);
-                // todoItem.doneButton.addEventListener('click', function () {
-                //     todoArray = JSON.parse(localStorage.getItem(listName));
-                //     todoItem.item.classList.toggle('list-group-item-success');
-                //     changeItemStatus(todoArray, todoItem.item);
-                //     localStorage.setItem(listName, JSON.stringify(todoArray));
-                // });
-
-                // todoItem.deleteButton.addEventListener('click', function () {
-                //     if (confirm('Вы уверены?')) {
-                //         todoArray = JSON.parse(localStorage.getItem(listName));
-                //         let newArray = searchDeleteItem(todoArray, todoItem.item);
-                //         localStorage.setItem(listName, JSON.stringify(newArray));
-                //         todoItem.item.remove();
-                //     }
-                // });
-
-                todoList.append(todoItem.item);
-                todoItem.item.append(todoItem.buttonGroup);
-
-            }
+        if (todoItem.done) {
+            item.classList.add('list-group-item-success');
         }
+
 
         function searchItemStatus(array, item) {
             array.map((obj) => {
@@ -134,36 +83,129 @@
 
         function changeItemStatus(item, button) {
             button.addEventListener('click', function () {
-                todoArray = JSON.parse(localStorage.getItem(listName));
-                item.classList.toggle('list-group-item-success');
-                searchItemStatus(todoArray, item);
-                localStorage.setItem(listName, JSON.stringify(todoArray));
+                onDone({ todoItem, element: item });
+                // todoArray = JSON.parse(localStorage.getItem(listName));
+                item.classList.toggle('list-group-item-success', todoItem.done);
+                // searchItemStatus(todoArray, item);
+                // localStorage.setItem(listName, JSON.stringify(todoArray));
             });
         }
 
         function deleteItem(item, button) {
             button.addEventListener('click', function () {
-                if (confirm('Вы уверены?')) {
-                    todoArray = JSON.parse(localStorage.getItem(listName));
-                    let newArray = searchDeleteItem(todoArray, item);
-                    localStorage.setItem(listName, JSON.stringify(newArray));
-                    item.remove();
-                }
+                onDelete({ todoItem, element: item });
+                // if (confirm('Вы уверены?')) {
+                //     todoArray = JSON.parse(localStorage.getItem(listName));
+                //     let newArray = searchDeleteItem(todoArray, item);
+                //     localStorage.setItem(listName, JSON.stringify(newArray));
+                //     item.remove();
+                // }
             });
         }
 
+        changeItemStatus(item, doneButton);
+        deleteItem(item, deleteButton);
 
-        todoItemForm.form.addEventListener('submit', function (evt) {
+        buttonGroup.append(doneButton, deleteButton);
+        item.append(buttonGroup);
+
+        return item;
+    }
+
+    async function createTodoApp(container, title, owner) {
+        let todoAppTitle = createAppTitle(title);
+        let todoItemForm = createTodoForm();
+        let todoList = createTodoList();
+        const handlers = {
+            onDone({ todoItem }) {
+                todoItem.done = !todoItem.done;
+                fetch(`http://localhost:3000/api/todos/${todoItem.id}`, {
+                    method: 'PATCH',
+                    body: JSON.stringify({ done: todoItem.done }),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                });
+            },
+            onDelete({ todoItem, element }) {
+                if (confirm('Вы уверены?')) {
+                    element.remove();
+                    fetch(`http://localhost:3000/api/todos/${todoItem.id}`, {
+                        method: 'DELETE',
+                    });
+                    if (response.status === 404)
+                        console.log('Не удалось удалить дело, так как его не существует');
+                }
+            }
+        }
+
+        container.append(todoAppTitle, todoItemForm.form, todoList);
+
+        const response = await fetch(`http://localhost:3000/api/todos?owner=${owner}`);
+        const todoItemList = await response.json();
+
+        todoItemList.forEach((todoItem) => {
+            const todoItemElement = createTodoItemElement(todoItem, handlers);
+            todoList.append(todoItemElement);
+        })
+
+        // if (localStorage.getItem(listName)) {
+        //     todoArray = JSON.parse(localStorage.getItem(listName));
+
+        //     for (let object of todoArray) {
+        //         let todoItem = createTodoItem(todoItemForm.input.value);
+        //         todoItem.item.id = object.id;
+        //         todoItem.item.textContent = object.name;
+        //         if (object.done == true) {
+        //             todoItem.item.classList.add('list-group-item-success');
+        //         } else {
+        //             todoItem.item.classList.remove('list-group-item-success');
+        //         }
+
+
+        //         changeItemStatus(todoItem.item, todoItem.doneButton);
+        //         deleteItem(todoItem.item, todoItem.deleteButton);
+        //         // todoItem.doneButton.addEventListener('click', function () {
+        //         //     todoArray = JSON.parse(localStorage.getItem(listName));
+        //         //     todoItem.item.classList.toggle('list-group-item-success');
+        //         //     changeItemStatus(todoArray, todoItem.item);
+        //         //     localStorage.setItem(listName, JSON.stringify(todoArray));
+        //         // });
+
+        //         // todoItem.deleteButton.addEventListener('click', function () {
+        //         //     if (confirm('Вы уверены?')) {
+        //         //         todoArray = JSON.parse(localStorage.getItem(listName));
+        //         //         let newArray = searchDeleteItem(todoArray, todoItem.item);
+        //         //         localStorage.setItem(listName, JSON.stringify(newArray));
+        //         //         todoItem.item.remove();
+        //         //     }
+        //         // });
+
+        //         todoList.append(todoItem.item);
+        //         todoItem.item.append(todoItem.buttonGroup);
+
+        //     }
+        // }
+
+        todoItemForm.form.addEventListener('submit', async function (evt) {
             evt.preventDefault();
-
-            let todoItem = createTodoItem(todoItemForm.input.value);
 
             if (!todoItemForm.input.value) {
                 return;
             }
 
-            changeItemStatus(todoItem.item, todoItem.doneButton);
-            deleteItem(todoItem.item, todoItem.deleteButton);
+            const response = await fetch('http://localhost:3000/api/todos', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    name: todoItemForm.input.value.trim(),
+                    owner
+                })
+            });
+            const todoItem = await response.json();
+            const todoItemElement = createTodoItemElement(todoItem, handlers);
 
             // todoItem.doneButton.addEventListener('click', function () {
             //     todoArray = JSON.parse(localStorage.getItem(listName));
@@ -181,27 +223,27 @@
             //     }
             // });
 
-            let localStorageData = localStorage.getItem(listName);
-            if (localStorageData == null) {
-                todoArray = [];
-            } else {
-                todoArray = JSON.parse(localStorageData);
-            };
+            // let localStorageData = localStorage.getItem(listName);
+            // if (localStorageData == null) {
+            //     todoArray = [];
+            // } else {
+            //     todoArray = JSON.parse(localStorageData);
+            // };
 
-            function createTodoItemObject(array) {
-                const itemObject = {
-                    id: todoItem.item.id,
-                    name: todoItemForm.input.value,
-                    done: false,
-                };
+            // function createTodoItemObject(array) {
+            //     const itemObject = {
+            //         id: todoItem.item.id,
+            //         name: todoItemForm.input.value,
+            //         done: false,
+            //     };
 
-                array.push(itemObject);
-            };
+            //     array.push(itemObject);
+            // };
 
-            createTodoItemObject(todoArray);
-            localStorage.setItem(listName, JSON.stringify(todoArray));
+            // createTodoItemObject(todoArray);
+            // localStorage.setItem(listName, JSON.stringify(todoArray));
 
-            todoList.append(todoItem.item);
+            todoList.append(todoItemElement);
             todoItemForm.input.value = '';
             todoItemForm.button.disabled = true;
         });
